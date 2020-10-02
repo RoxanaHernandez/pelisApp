aad1d03f8a28903e2d8a06f6b815ce2a9d55bcf0
RoxanaHernandez <roxanahernandez326@gmail.com>
2020-10-02 16:41:26 -0300

    'organization'

--- pelisapp.js
+++ pelisapp.js
@@ -5,14 +5,11 @@ const descripcion = document.getElementById("descripcion")
 const califi = document.getElementById("califi")
 const enviar = document.querySelector('#enviar')
 
-enviar.addEventListener("focus", function (){
-console.log("hiciste foco")
-
-})
-enviar.addEventListener("blur", function (){
-    console.log("te fuiste del input")
     
-    })
+
+
+    
+    
 
 enviar.addEventListener("click", function (event){
     peliculas.push({
@@ -29,32 +26,48 @@ enviar.addEventListener("click", function (event){
     event.preventDefault();
     return false;
     
-
+   
 
 })
 
 
-var peliculas = []
+var peliculas = [
+
+]
+
+
 if(localStorage.getItem("peliculas")){
     peliculas=JSON.parse(localStorage.getItem("peliculas"))
 }
 
 function generarHtmlDePeli(peli){
-    return `<div>
-    <div>
-        <img src="${peli.imagen}" alt="">
-    </div>
-    <div>
-        <h3>Titulo</h3>
-        <p>${peli.titulo}
-        </p>
-        <h4>Genero</h4>
-        <p>${peli.genero}</p>
-        <h4>Descripcion</h4>
-        <p>${peli.descripcion}</p>
-    </div>
-    
-</div>`
+    return `<article class="peliculas">
+                <div class="peliculas-container">
+                    <div>
+                        <img class="poster"  ${peli.imagen}>
+                
+                    </div>
+                    <div class="derecha">
+                        <h3>Titulo</h3>
+                        <p>${peli.titulo}</p>
+                        <h4>Genero</h4>
+                        <p>${peli.genero}</p>
+                        <h4>Descripcion</h4>
+                        <p>${peli.descripcion}</p>
+                    </div>
+                    
+                </div>
+                <div>
+                    <div>
+                        <h4>Calificacion</h4>
+                        <p>&#9734; &#9734; &#9734; &#9734; &#9734; </p>
+                    </div>
+                    <div class="peliculas-boton">
+                        <button>Editar</button>
+                        <button>Eliminar</button>
+                    </div>
+                </div>
+            </article>`
 }
 
 var div = document.createElement("div");
@@ -74,3 +87,18 @@ div.innerHTML = str;
 document.body.appendChild(div);
 
 recorrePeli()
+//function limpiarFormulario() {
+  //  document.getElementById("formulario").reset();
+  //}
+  //<input type="button" onclick="limpiarFormulario()" value="Limpiar formulario">
+  //function limpiarYEnviar() {
+    //limpiarFormulario();
+    //return true;
+  //}
+  //cdnjs libreria
+  /* LINKEA POR URL EL JQUERY Y NO DESCARGA EL ARCHIVO
+  <script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script> */
+  /* slide para que despliegue mi form con jquery callback de fade  animate scrolltop*/
